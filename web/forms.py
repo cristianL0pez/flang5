5c5fa0e78e7fe5410c1b338e477086e4af9ddf5f
cristian_lopez <cristianlopezvalpo@gmail.com>
2024-05-03 18:41:23 -0400

    hito 2

---
+++ web/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Contacto
+
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contacto
+        fields = ['customer_name','customer_email','message']
